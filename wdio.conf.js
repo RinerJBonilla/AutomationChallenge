a4fdbf136060b40feed349b6f3f2dae6b0ef48e7
RinerJBonilla <riner.bonilla@unitec.edu>
2020-07-31 01:07:30 -0600

    save junit reports on xml for jenkins

--- wdio.conf.js
+++ wdio.conf.js
@@ -124,7 +124,7 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter.html
-  reporters: ["spec"],
+  reporters: ["spec", ["junit", { outputDir: "./" }]],
 
   //
   // If you are using Cucumber you need to specify the location of your step definitions.
